3227f63584d26830c299b881be922b02495cf9e0
PiniShlomi <pini.shlomi@grid4c.com>
2024-05-01 10:14:29 +0300

    Initial commit

---
+++ code/lesson1/Main.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+/**
+ * Created by Pini Shlomi At 01/05/2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
